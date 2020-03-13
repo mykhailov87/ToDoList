4afcf0b41505fccd6e50fbff59cd98bcd0b7add6
serhii <mikhajlov87@gmail.com>
2020-03-13 21:59:57 +0200

    I have seen that

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import axios from 'axios';
 import Modal from 'react-modal';
 import './main.css';
 
-Modal.setAppElement('#root')
+Modal.setAppElement('#root');
 
-function Main(){
-  
+function Main() {
   let [data, setData] = useState([]);
   let [postData, setPostData] = useState('');
   let [deleteData, setDeleteData] = useState();
   let [patchDataId, setPatchDataId] = useState();
   let [patchData, setPatchData] = useState('');
-  
+
   const customStyles = {
     content : {
       top                   : '50%',
@@ -24,85 +23,68 @@ function Main(){
     }
   };
 
-  
-
   useEffect(() => {
-    axios.get('http://localhost:3000/posts')
-    .then(function (response) {
-      const post = response.data;
-      setData(post);
-      console.log(post);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-    // TODO: add server request
-    // TODO: save data
+    axios.get('http://localhost:3004/posts')
+      .then((response) => {
+        const post = response.data;
+        setData(post);
+        console.log(post);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+      // TODO: add server request
+      // TODO: save data
   }, []);
-  
-  const handleChange = function (ev) {
+
+  const handleChange = useCallback((ev) => {
     const post = ev.target.value;
     setPostData(post);
-  }
+  }, []);
 
-  const onClickDelete = function (ev) {
+  const onClickDelete = useCallback((ev) => {
     const id = ev.currentTarget.id;
     setDeleteData(id);
-  }
+  }, []);
 
-  const handleChangePatch = function (ev) {
+  const handleChangePatch = useCallback((ev) => {
     const patch = ev.target.value;
     setPatchData(patch);
-  }
+  }, []);
 
-  function handleSubmit (event) {
+  const handleSubmit = useCallback((event) => {
     event.preventDefault();
 
-    axios.post(`http://localhost:3000/posts`, { 
+    axios.post(`http://localhost:3004/posts`, {
       title: postData,
       author: 'Bogdan'
      })
-     .then(function () {
-      return axios.get('http://localhost:3000/posts')
-    })
-    .then(function (response) {
-      const post = response.data;
-      setData(post);
-      console.log(post);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-  }
+      .then(() => axios.get('http://localhost:3004/posts'))
+      .then((response) => {
+        const post = response.data;
+        setData(post);
+      })
+      .catch((error) => {
+        // handle error
+        console.log(error);
+      })
+  }, [postData]);
 
-  function handleSubmitDelete (event) {
+  const handleSubmitDelete = useCallback((event) => {
     event.preventDefault();
 
-    axios.delete(`http://localhost:3000/posts/${deleteData}`)
-     .then(function () {
-      return axios.get('http://localhost:3000/posts')
-    })
-    .then(function (response) {
-      const post = response.data;
-      setData(post);
-      console.log(post);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-  }
+    // axios.delete(`http://localhost:3004/posts/${deleteData}`)
+    //   .then(() => axios.get('http://localhost:3004/posts'))
+    //   .then((response) => {
+    //     const post = response.data;
+    //     setData(post);
+    //   })
+    //   .catch((error) => {
+    //     // handle error
+    //     console.log(error);
+    //   })
+  }, [deleteData]);
 
   function handleSubmitPatch (event) {
     event.preventDefault();
@@ -129,19 +111,19 @@ function Main(){
     setIsOpen(false);
   }
 
-  
+
   const [modalIsOpen,setIsOpen] = React.useState(false);
   function openModal(ev) {
     const id = ev.currentTarget.id;
     setPatchDataId(id);
     setIsOpen(true);
   }
- 
+
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    
+
   }
- 
+
   function closeModal(){
     setIsOpen(false);
   }
@@ -149,59 +131,67 @@ function Main(){
   return(
     <div className="main">
       <form className="inputfield" onSubmit={handleSubmit}>
-        <input 
+        <input
           onChange = {handleChange}
           type="text"
-          id="input" 
+          id="input"
           placeholder="What shall I do today?"
           value={postData}
         />
         <button>+</button>
       </form>
-      
+
       <div id="textfield">
-        {data.map((post) => 
-          <form onSubmit={handleSubmitDelete}>
-            <p id={post.id}
-               title={post.title}>
-                 {post.title}
+        {data.map((post) =>
+          <form key={post.id} onSubmit={handleSubmitDelete}>
+            <p id={post.id} title={post.title}>
+               {post.title}
             </p>
-            <button onClick={openModal}
-                    id={post.id}>
-                    EDIT
+            <button onClick={openModal} id={post.id}>
+              EDIT
             </button>
             <Modal
               isOpen={modalIsOpen}
               onAfterOpen={afterOpenModal}
               onRequestClose={closeModal}
               style={customStyles}
-              
+
               contentLabel="Example Modal"
             >
- 
-              
               <button onClick={closeModal}>close</button>
               <form onSubmit={handleSubmitPatch}>
                 <input
                   onChange = {handleChangePatch}
                   type="text"
-                  id="input" 
+                  id="input"
                   placeholder="What shall I do today?"
-                  value={patchData} 
+                  value={patchData}
                 />
                 <button>Apply</button>
               </form>
             </Modal>
-            <button type="submit" 
-                    id={post.id}
-                    onClick={onClickDelete}>
-                    DELETE
-            </button>
+            <SubmitButton
+              id={post.id}
+              onClickHandler={onClickDelete}
+            />
           </form>
         )}
       </div>
     </div>
   );
-};
+}
+
+function SubmitButton(props) {
+  const { id, onClickHandler } = props;
+  return (
+    <button
+      type="submit"
+      id={id}
+      onClick={onClickHandler}
+    >
+      DELETE
+    </button>
+  );
+}
 
 export default Main;
